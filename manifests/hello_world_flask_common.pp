cb5af305f944818191e96f8315859f24cd17f0de
PrinceSalatyell <salatyelll14@gmail.com>
2024-02-19 01:18:53 +0000

    issue #5: trying to make the flask apps work with python3-gunicorn package

--- manifests/hello_world_flask_common.pp
+++ manifests/hello_world_flask_common.pp
@@ -5,11 +5,7 @@ class puppet_infrastructure::hello_world_flask_common {
   $localdir            = lookup('filesystem::localdir')
 
   # Packages needed for the applications to run
-  if $os_family == 'Debian' {
-    package { [ 'gunicorn', 'python3-flask' ]: ensure => present, }
-  } else {
-    package { [ 'python3-gunicorn', 'python3-flask' ]: ensure => present, } 
-  }
+  package { [ 'python3-gunicorn', 'python3-flask' ]: ensure => present, }
 
 
   # Directory for the hello world like demo applications
