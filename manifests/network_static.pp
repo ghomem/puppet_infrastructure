f807e7bed71d4879359fc6ffda23fe4b73ab5ed3
Gustavo Homem <homem.gustavo@gmail.com>
2024-02-27 13:57:27 +0100

    fix file mixup + remove port configuration

--- manifests/network_static.pp
+++ manifests/network_static.pp
@@ -1,80 +1,15 @@
 ### Purpose ########
-# This class configures an OpenVPN server with user auth and
-# and an internal caching nameserver for DNS resolution to be
-# provided independently from the VPN client network.
+# This class configures a network interface with static parameters.
 #
-# It expects OpenVPN SSL environment specific files at: 
-#
-#   puppet://extra_files/openvpn/keys/server.crt
-#   puppet://extra_files/openvpn/keys/server.key
-#   puppet://extra_files/openvpn/keys/ca.crt
-#   puppet://extra_files/openvpn/keys/dh2048.pem
+# It was originally created because the OpenVPN server class
+# needs to manage interface lo and it has been found that
+# once puppet starts managing one interface it has to manage
+# all of them.
 
-class network_vpn (
-  Boolean $client_isolation = true,
-  $wan_iface                = 'eth0', 
-  $openvpn_lan_iface        = 'lo:0',
-  $openvpn_bridge_iface     = 'br0',
-  $openvpn_port             = '1194',
-  $openvpn_reneg_sec        = 3600,
-  $openvpn_static_ip        = '192.168.101.1',
-  $openvpn_netmask          = '255.255.255.0',
-  $openvpn_network          = '192.168.101.0',
-  $openvpn_broadcast        = '192.168.101.255',
-  $openvpn_pool_start       = '192.168.101.101',
-  $openvpn_pool_end         = '192.168.101.200',
-  $openvpn_cipher           = 'AES-256-CBC',
-) {
+class puppet_infrastructure::network_static ( $iface, $ipaddress, $gateway ) {
 
-    # Caching nameserver for the VPN
-
-    class { 'puppet_infrastructure::caching_nameserver':
-        allowed_networks => ["${openvpn_network}/24"]
-    }
-
-    class { 'puppet_infrastructure::firewall_addon_caching_nameserver':
-        lan_iface => $openvpn_bridge_iface
-    }
-
-    # OpenVPN specific firewall rules
-
-    class { 'puppet_infrastructure::firewall_addon_openvpn_server':
-        wan_iface        => $wan_iface,
-        lan_bridge_iface => $openvpn_bridge_iface,
-        port             => $openvpn_port,
-    }
-
-    # expects OpenVPN SSL files at: puppet://extra_files/openvpn/keys
-    class { 'puppet_infrastructure::openvpn_server':
-        lan_static_ip    => $openvpn_static_ip,
-        lan_netmask      => $openvpn_netmask,
-        lan_network      => $openvpn_network,
-        lan_broadcast    => $openvpn_broadcast,
-        vpn_pool_start   => $openvpn_pool_start,
-        vpn_pool_end     => $openvpn_pool_end,
-        lan_iface        => $openvpn_lan_iface,
-        lan_bridge_iface => $openvpn_bridge_iface,
-        push_dns_server  => $openvpn_static_ip,
-        cipher           => $openvpn_cipher,
-        reneg_sec        => $openvpn_reneg_sec,
-        client_isolation => $client_isolation,
-    }
+    package        { 'ifupdown-extra': ensure => 'installed', }
+    network_config { $iface:           ensure => 'present', family  => 'inet',   onboot    => 'true', method => 'static',   ipaddress => $ipaddress, hotplug => 'false' }
+    network_route  { 'default':        ensure => 'present', gateway => $gateway, interface => 'eth0', netmask => '0.0.0.0', network   => 'default' }
 
 }
-
-node 'vpn' {
-
-  include puppet_infrastructure::node_base
-  include passwd_common
-
-  $my_iface = $::networking['primary']
-
-  # TODO: move to puppet infrastructure
-  class { 'network_dhcp': iface     => $my_iface }
-  class { 'network_vpn' : wan_iface => $my_iface}
-
-
-  include puppet_infrastructure::firewall_addon_openvpn_server
-
-}
-
