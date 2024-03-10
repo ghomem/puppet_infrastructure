07f3bb292392bb9de7289b271762d1a326b641d9
PrinceSalatyell <salatyelll14@gmail.com>
2024-03-10 17:50:04 +0000

    issue #31: Adapting our code to the small patch that prevents the docker container to redownload on every agent run

--- manifests/hello_world_docker.pp
+++ manifests/hello_world_docker.pp
@@ -12,11 +12,13 @@ class puppet_infrastructure::hello_world_docker (
 
   # runs the container and creates service
   docker::run { 'flask-helloworld':
-    ensure    => present,
-    image     => $img_name,
-    env       => [ ],
-    ports     => [ "${myport}:5000" ],
-    subscribe => Docker::Image[ $img_name ],
+    ensure                            => present,
+    image                             => $img_name,
+    env                               => [ ],
+    ports                             => [ "${myport}:5000" ],
+    remove_container_on_stop          => false,
+	  restart_service_on_docker_refresh => true,
+    subscribe                         => Docker::Image[ $img_name ],
   }
 
 }
