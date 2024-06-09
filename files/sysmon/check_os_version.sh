aa65f4a4d1188430785aeb2cce18c9c2595cfbb2
PrinceSalatyell <salatyelll14@gmail.com>
2024-06-09 10:52:10 +0100

    issue #39: updating some sysmon checks to support rhel

--- files/sysmon/check_os_version.sh
+++ files/sysmon/check_os_version.sh
@@ -1,9 +1,20 @@
-#/bin/sh
+#!/bin/sh
 
 set -e
 
-OS=`lsb_release -is`
-VERSION=`lsb_release -rs`
+# Check if lsb_release command is available
+if command -v lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -is)
+    VERSION=$(lsb_release -rs)
+# Check if /etc/os-release file exists
+elif [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VERSION=$VERSION_ID
+else
+    echo "UNKNOWN - OS information not found"
+    exit 3
+fi
 
 STATE="OK - OS: ${OS} ${VERSION}"
 status=0
