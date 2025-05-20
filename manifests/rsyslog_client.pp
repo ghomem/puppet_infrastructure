f2702ed6585eb4c952b4d481f953c5af4c6185d8
PrinceSalatyell <salatyelll14@gmail.com>
2025-05-20 15:43:05 +0100

    issue #69: add the new classes

---
+++ manifests/rsyslog_client.pp
@@ -0,0 +1,28 @@
+class puppet_infrastructure::rsyslog_client (
+  String  $target,
+  Integer $port         = 6514,
+  Boolean $use_failover = false,
+  Optional[String] $failover = undef,
+) {
+
+  include rsyslog
+  package { 'rsyslog-gnutls': ensure => installed }
+
+  $ca_file   = '/etc/puppetlabs/puppet/ssl/certs/ca.pem'
+  $cert_file = "/etc/puppetlabs/puppet/ssl/certs/${facts['fqdn']}.pem"
+  $key_file  = "/etc/puppetlabs/puppet/ssl/private_keys/${facts['fqdn']}.pem"
+
+  file { '/etc/rsyslog.d/40-forward.conf':
+    mode    => '0644',
+    content => epp('puppet_infrastructure/rsyslog/forward_simple.conf.epp', {
+                    target       => $target,
+                    failover     => $failover,
+                    use_failover => $use_failover,
+                    ca_file      => $ca_file,
+                    cert_file    => $cert_file,
+                    key_file     => $key_file,
+                    port         => $port,
+                  }),
+    notify  => Service['rsyslog'],
+  }
+}
