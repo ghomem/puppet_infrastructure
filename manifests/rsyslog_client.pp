8a5df36c268a0b6561c5a9f3ef9d84732251aaef
PrinceSalatyell <salatyelll14@gmail.com>
2026-04-23 01:54:52 +0100

    Issue #69: Small cleanup patches to the client and server classes

--- manifests/rsyslog_client.pp
+++ manifests/rsyslog_client.pp
@@ -1,43 +1,41 @@
 class puppet_infrastructure::rsyslog_client (
-  String  $target,
-  String  $target_ip,
-  Integer $port         = 6514,
-  Boolean $check_names  = false,   # true → require hostname == cert CN
-  Optional[String] $failover      = undef,
-  Optional[String]  $failover_ip = undef,
+  String            $target,
+  Optional[String]  $target_ip    = undef,
+  Integer           $port         = 6514,
+  Boolean           $check_names  = false,
+  Optional[String]  $failover     = undef,
+  Optional[String]  $failover_ip  = undef,
 ) {
 
   include puppet_infrastructure::rsyslog_base
 
   $certname = $trusted['certname']
 
-  # guarantee the name is always resolvable
-  if $target_ip {
+  if $target_ip != undef {
     host { $target:
       ip     => $target_ip,
       target => '/etc/hosts',
     }
   }
 
-  if $failover {
-    if $failover_ip {
-      host { $failover:
-        ip     => $failover_ip,
-        target => '/etc/hosts',
-      }
+  if $failover != undef and $failover_ip != undef {
+    host { $failover:
+      ip     => $failover_ip,
+      target => '/etc/hosts',
     }
   }
 
   file { '/etc/rsyslog.d/40-forward.conf':
     content => epp('puppet_infrastructure/rsyslog/forward_simple.conf.epp', {
-      target       => $target,
-      port         => $port,
-      certname     => $certname,
-      check_names  => $check_names,
-      failover     => $failover,
+      target      => $target,
+      port        => $port,
+      certname    => $certname,
+      check_names => $check_names,
+      failover    => $failover,
     }),
-    owner  => 'root', group => 'root', mode => '0644',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
     notify => Service['rsyslog'],
   }
 }
-
