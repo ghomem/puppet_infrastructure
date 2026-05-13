e2037c24b4fea760e3bfeb7d32b0265d3562d5eb
Gustavo Homem <gustavo.homem@meaningfuldata.eu>
2026-05-14 00:22:31 +0200

    experimental change

--- manifests/rsyslog_client.pp
+++ manifests/rsyslog_client.pp
@@ -2,12 +2,13 @@
 
 class puppet_infrastructure::rsyslog_client (
   String            $target,
-  Optional[String]  $target_ip       = undef,
-  Integer           $port            = 6514,
-  Boolean           $check_names     = false,
-  Optional[String]  $failover        = undef,
-  Optional[String]  $failover_ip     = undef,
-  Optional[String]  $ssldir_override = undef,
+  Optional[String]  $target_ip        = undef,
+  Integer           $port             = 6514,
+  Boolean           $check_names      = false,
+  Optional[String]  $failover         = undef,
+  Optional[String]  $failover_ip      = undef,
+  Optional[String]  $ssldir_override  = undef,
+  Boolean           $forward_auth_log = false,
 ) {
 
   # Reuse the shared rsyslog setup; allow the SSL dir to be overridden when needed.
@@ -40,11 +41,12 @@ class puppet_infrastructure::rsyslog_client (
 
   file { '/etc/rsyslog.d/40-forward.conf':
     content => epp('puppet_infrastructure/rsyslog/forward_simple.conf.epp', {
-      target      => $target,
-      port        => $port,
-      certname    => $certname,
-      check_names => $check_names,
-      failover    => $failover,
+      target           => $target,
+      port             => $port,
+      certname         => $certname,
+      check_names      => $check_names,
+      failover         => $failover,
+      forward_auth_log => $forward_auth_log,
     }),
     owner  => 'root',
     group  => 'root',
